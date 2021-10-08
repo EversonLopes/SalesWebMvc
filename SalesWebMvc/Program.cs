4ee2cd678740ca22cb23d23ef9af9c4ec53ae100
Everson Lopes <everrl22@hotmail.com>
2021-10-08 16:59:13 -0300

    Project created

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -16,6 +16,7 @@ namespace SalesWebMvc
         {
             CreateWebHostBuilder(args).Build().Run();
         }
+        //
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
